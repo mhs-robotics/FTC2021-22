a6b88cf4f1c46ad97afc0f57935120468c741109
Miles Contopoulos <milesdabuilda@gmail.com>
2021-12-21 16:08:40 -0500

    Deleted a file and made opmode move actually work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeMecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeMecanum.java
@@ -6,24 +6,47 @@ import com.qualcomm.robotcore.hardware.*;
 @TeleOp
 public class OpModeMecanum extends OpMode {
   public static DcMotor motorFrontLeft;
+  public static DcMotor motorBackLeft;
+  public static DcMotor motorFrontRight;
+  public static DcMotor motorBackRight;
+  public double speed = .7;
 
   @Override
   public void init() {
-    telemetry.addData("Status", "Mecanum Move Initiated");
-
-    motorFrontLeft = hardwareMap.dcMotor.get("FrontL");
+    motorFrontLeft = hardwareMap.get(DcMotor.class, "FrontL");
+    motorBackLeft = hardwareMap.get(DcMotor.class, "BackL");
+    motorFrontRight = hardwareMap.get(DcMotor.class, "FrontR");
+    motorBackRight = hardwareMap.get(DcMotor.class, "BackR");
 
     motorFrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+    motorBackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
 
+    telemetry.addData("Status", "Mecanum Move Initiated");
     telemetry.addData("Status", "Op Mode Control Initiated");
   }
 
   @Override
   public void loop(){
-    double x = -gamepad1.left_stick_y * 1.1;
-    double y = gamepad1.left_stick_x;
+    double y = -gamepad1.left_stick_y;
+    double x = gamepad1.left_stick_x;
     double rx = gamepad1.right_stick_x;
 
-    mecanumMove.move(x, y, rx);
+    if(Math.abs(x) < .3){x = 0;}
+    if(Math.abs(y) < .3){y = 0;}
+
+    telemetry.addData("X", x);
+    telemetry.addData("Y", y);
+    telemetry.update();
+
+    double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+    double frontLeftPower = (y + x + rx) * speed / denominator;
+    double backLeftPower = (y - x + rx) * speed / denominator;
+    double frontRightPower = (y - x - rx) * speed / denominator;
+    double backRightPower = (y + x - rx) * speed / denominator;
+
+    motorFrontLeft.setPower(frontLeftPower);
+    motorBackLeft.setPower(backLeftPower);
+    motorFrontRight.setPower(frontRightPower);
+    motorBackRight.setPower(backRightPower);
   }
 }
